dd8bbc60d06c107b6958bdebf1bb36ebe6b373e0
SuzumeJr <suzume-jr@mbn.nifty.com>
2020-08-08 19:56:03 +0900

    Chromiumベース版Edgeのwebview２を使用しC#で作成したTweetDeckビュアー

---
+++ TweetDeckEdge/ExtentionTweetDeckEdge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ExtentionTweetDeckEdge
+{
+    static public class ExtentionMethod
+    {
+        static public int ToInt(this string value)
+        {
+            return int.Parse(value);
+        }
+
+        static public double ToDouble(this string value)
+        {
+            return double.Parse(value);
+        }
+    }
+}
---
+++ TweetDeckEdge/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+
+namespace TweetDeckEdge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadConfig();
+            webView.Source = new Uri(Properties.Settings.Default.WebViewSource);
+        }
+
+        private void webView_CoreWebView2Ready(object sender, EventArgs e)
+        {
+            webView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+
+            webView.CoreWebView2.NewWindowRequested += (sender, e) =>
+            {
+                var openPath = new Uri(e.Uri);
+
+                if ("http" == openPath.Scheme || "https" == openPath.Scheme)
+                {
+                    var proc = new System.Diagnostics.Process();
+
+                    proc.StartInfo.FileName = e.Uri;
+                    proc.StartInfo.UseShellExecute = true;
+                    proc.Start();
+                }
+
+                e.GetDeferral();
+                e.Handled = true;
+            };
+        }
+
+        private void LoadConfig()
+        {
+            // 設定値の取得
+            Application.Current.MainWindow.Height = Properties.Settings.Default.WindowsPositionHeight;
+            Application.Current.MainWindow.Width = Properties.Settings.Default.WindowsPositionWidth;
+            Application.Current.MainWindow.Top = Properties.Settings.Default.WindowsPositionTop;
+            Application.Current.MainWindow.Left = Properties.Settings.Default.WindowsPositionLeft;
+        }
+
+        private void SaveConfig()
+        {
+            Properties.Settings.Default.WindowsPositionHeight = Application.Current.MainWindow.Height;
+            Properties.Settings.Default.WindowsPositionWidth = Application.Current.MainWindow.Width;
+            Properties.Settings.Default.WindowsPositionTop = Application.Current.MainWindow.Top;
+            Properties.Settings.Default.WindowsPositionLeft = Application.Current.MainWindow.Left;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SaveConfig();
+        }
+    }
+}
